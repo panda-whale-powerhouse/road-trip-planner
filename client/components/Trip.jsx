0d4ea5a844faa6912f7e6a37dd08577c1ef79a77
agrimm17 <alexandergrimm17@gmail.com>
2024-09-25 17:26:29 -0700

    Made Trip Buttons clickable and repopulate the Settings text inputs

--- client/components/Trip.jsx
+++ client/components/Trip.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  updateName,
+  updateDestination,
+  updateOrigin,
+  updateStep,
+  updateWaypoints,
+} from '../reducers/genSettingsSlice';
 
 const Trip = ({ trip }) => {
-  const { roadtripName, tripData } = trip;
+  const dispatch = useDispatch();
+  const { name, destination, origin, step, waypoints, waypointStr } =
+    trip.tripData;
+
+  function selectTrip() {
+    dispatch(updateName(name));
+    dispatch(updateOrigin(origin));
+    dispatch(updateDestination(destination));
+    dispatch(updateStep(step));
+    dispatch(updateWaypoints(waypoints));
+    //make call to update reducer to selected tripdata
+  }
 
   return (
-    <div>
-      <p>{roadtripName}</p>
+    <button onClick={selectTrip}>
+      <p>{name}</p>
       <p>
-        {tripData.origin} to {tripData.destination}
+        {origin} to {destination}
       </p>
-    </div>
+    </button>
   );
 };
 
